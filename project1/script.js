abb439cbb279f91ec32ad34f481681c85089f0c5
Mohammed Allibalogun <mallibalogun18@gmail.com>
2024-06-07 22:38:09 -0400

    basic on how to set id for each element and utilizing the document to manipulate the html file content.

--- project1/script.js
+++ project1/script.js
@@ -1,1 +1,14 @@
-console.log("Welcome to JavaScript");
+console.log("Welcome to JavaScript Tutorial.");
+
+
+// var box = document.querySelector("p");
+
+// box.addEventListener("click", function (){
+//     window.alert(`This is an alert!`)
+// })
+
+
+document.getElementById("myH1").textContent = "Welcome To JavaScript";
+document.getElementById("myIp").innerHTML = "<strong>Loreal</strong>";
+
+
